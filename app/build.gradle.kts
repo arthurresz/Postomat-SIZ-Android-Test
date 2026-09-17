d049affc6105ab72c041debe3f6da5b1f5418de0
arthurresz <arthur.resz@gmail.com>
2026-09-17 23:20:53 +0300

    Set v3.1 classic UI build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.rsteel.postomatsiz.saftest"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0-standard-admin-beta"
+        versionCode = 4
+        versionName = "3.1-standard-classic-ui"
     }
 
     buildTypes {
