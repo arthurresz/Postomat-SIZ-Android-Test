2674de7538de3bc97405ff063e5cbc78e31758b9
arthurresz <arthur.resz@gmail.com>
2026-09-17 22:36:03 +0300

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.rsteel.postomatsiz.saftest"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.rsteel.postomatsiz.saftest"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0-standard-saf-test"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
