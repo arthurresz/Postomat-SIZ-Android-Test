466a5809b1485ede31fb6201e914b5183963739a
arthurresz <arthur.resz@gmail.com>
2026-09-18 13:36:03 +0300

    Set v3.3 email and backup build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.rsteel.postomatsiz.saftest"
         minSdk = 23
         targetSdk = 35
-        versionCode = 5
-        versionName = "3.2-standard-classic-ui-labels"
+        versionCode = 6
+        versionName = "3.3-standard-classic-ui-email-backup"
     }
 
     buildTypes {
