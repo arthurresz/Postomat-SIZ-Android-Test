9910d272c18e0ac3b0f185cbe21513c91209ca6c
arthurresz <arthur.resz@gmail.com>
2026-09-19 17:42:27 +0300

    Bump version to 3.29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.rsteel.postomatsiz.saftest"
         minSdk = 23
         targetSdk = 35
-        versionCode = 28
-        versionName = "3.28-standard-classic-ui-final-warehouse-report"
+        versionCode = 29
+        versionName = "3.29-standard-classic-ui-replenishment-data-fix"
     }
 
     buildTypes {
