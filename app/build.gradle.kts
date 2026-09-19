857c09570a5139a8c203eeee07a33992541acced
arthurresz <arthur.resz@gmail.com>
2026-09-19 17:08:34 +0300

    Bump version to 3.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.rsteel.postomatsiz.saftest"
         minSdk = 23
         targetSdk = 35
-        versionCode = 25
-        versionName = "3.25-standard-classic-ui-background-mail"
+        versionCode = 26
+        versionName = "3.26-standard-classic-ui-smtp-dns-fallback"
     }
 
     buildTypes {
