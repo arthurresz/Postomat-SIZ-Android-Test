1c9ace1445afa54754fa62a43cb8a62e3c1eb552
arthurresz <arthur.resz@gmail.com>
2026-09-19 19:32:31 +0300

    Bump version to 3.36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.rsteel.postomatsiz.saftest"
         minSdk = 23
         targetSdk = 35
-        versionCode = 35
-        versionName = "3.35-standard-classic-ui-warehouse-issued-reports"
+        versionCode = 36
+        versionName = "3.36-standard-classic-ui-consumables-report"
     }
 
     buildTypes {
